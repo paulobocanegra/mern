5c83db5a99ed8f76c41aa31841fcf265305bcbf6
paulobocanegra <paulobocanegra@gmail.com>
2020-10-25 20:49:45 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:pN26dpqo85aM6ZQU@cluster0.kmgr9.mongodb.net/PBDB?retryWrites=true&w=majority'
+    //Make sure this is your own unique string
+}
+
